ebdc08c344188135678e2097276c29816d2dc2ff
DannyZee35 <itsdanny.zee@gmail.com>
2023-07-04 22:49:47 +0500

    cors fixed

--- index.js
+++ index.js
@@ -6,22 +6,28 @@ const bodyParser = require('body-parser');
 const routes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
 const cors = require('cors');
-const fileUpload = require('express-fileupload')
+const fileUpload = require('express-fileupload');
+
 connectDB();
 const app = express();
 
 app.use(fileUpload({
     useTempFiles: true
-}))
+}));
 app.use(cors());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
- 
+
+// Set up CORS headers
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', 'https://university-frontend-rosy.vercel.app');
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+    next();
+});
+
 app.use(routes);
 app.use(courseRoutes);
 
-app.listen(
-    process.env.PORT,
-    () => console.log("Backend is running")
-)
+app.listen(process.env.PORT, () => console.log("Backend is running"));
