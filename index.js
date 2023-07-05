bf0449156f8b9adaf272d4e573e372dd7dbd7448
Danial Aziz <itsdanny.zee@gmail.com>
2023-07-06 00:31:16 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,20 +5,24 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const routes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
-const cors = require('cors');
-const fileUpload = require('express-fileupload');
-
+const fileUpload = require('express-fileupload')
 connectDB();
 const app = express();
+const cors = require('cors');
 
-app.use(fileUpload({ useTempFiles: true }));
-
+app.use(fileUpload({
+    useTempFiles: true
+}))
+ 
 app.use(cors());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
-
+ 
 app.use(routes);
 app.use(courseRoutes);
 
-app.listen(process.env.PORT, () => console.log('Backend is running'));
+app.listen(
+    process.env.PORT,
+    () => console.log("Backend is running")
+)
