2701c52d4300bb6b7f331f732386cff6e3af560b
Danial Aziz <itsdanny.zee@gmail.com>
2023-07-06 00:19:14 +0500

    Update index.js

--- index.js
+++ index.js
@@ -13,13 +13,15 @@ const app = express();
 app.use(fileUpload({
     useTempFiles: true
 }))
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-//     res.setHeader('Access-Control-Allow-Credentials', true);
-//     next();
-// });
+
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', 'https://university-frontend-rosy.vercel.app');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 const options = {
   origin: '*',
 
