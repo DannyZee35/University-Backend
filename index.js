c74ff3185453c241948169d31831f57e422d3ae4
DannyZee35 <itsdanny.zee@gmail.com>
2023-07-10 15:01:06 +0500

    vercel.json updated

--- index.js
+++ index.js
@@ -5,36 +5,16 @@ const connectDB = require('./config/db');
 const bodyParser = require('body-parser');
 const routes = require('./routes/authRoutes');
 const courseRoutes = require('./routes/courseRoutes');
-const cors = require('cors');
 const fileUpload = require('express-fileupload')
 connectDB();
 const app = express();
+const cors = require('cors');
 
 app.use(fileUpload({
     useTempFiles: true
 }))
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-const options = {
-  origin: '*',
-
-  methods: [
-    'GET',
-    'POST',
-    'PUT',
-    'DELETE'  
-  ],
-
-  allowedHeaders: [
-    'Content-Type',
-  ],
-};
-app.use(cors(options));
+ 
+app.use(cors());
 
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
